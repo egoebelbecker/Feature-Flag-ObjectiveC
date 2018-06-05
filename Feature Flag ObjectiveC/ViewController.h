bd3c986fd4fa04379af2b502d9599bd2e7f5e51c
Eric Goebelbecker <eric@ericgoebelbecker.com>
2018-06-04 20:52:33 -0400

    Initial Commit

---
+++ Feature Flag ObjectiveC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Feature Flag ObjectiveC
+//
+//  Created by Eric Goebelbecker on 6/4/18.
+//  Copyright © 2018 Eric Goebelbecker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Feature Flag ObjectiveC/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Feature Flag ObjectiveC
+//
+//  Created by Eric Goebelbecker on 6/4/18.
+//  Copyright © 2018 Eric Goebelbecker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
